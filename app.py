bb6097e7b85852428a1857c9f6940a8647d84858
Xiandi Pan <cindypan1807@gmail.com>
2023-04-19 20:39:21 -0400

    final

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def index():
+    """return prompt of madlids"""
+
+
+    prompt = silly_story.prompts
+
+    return render_template("questions.html",words=prompt)#naming
+
+@app.get("/results")
+def result():
+    """return re"""
+
+    story = silly_story.get_result_text(request.args)
+
+
+
+    return render_template("results.html",results = story)
